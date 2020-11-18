8f41fbb00f8bc070494935b0b67f7429ba092011
Andrew Bespaly <32086172+andrewbespaly@users.noreply.github.com>
2020-11-18 14:58:54 -0800

    Create teams.js

---
+++ nba-dashboard/src/components/teams.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {Doughnut} from 'react-chartjs-2';
+
+
+export default class Teams extends Component {
+    
+    render() {
+        return (
+          <div>
+              <h1>
+                NBA Teams
+              </h1>
+
+          </div>
+        );
+      }
+
+}
