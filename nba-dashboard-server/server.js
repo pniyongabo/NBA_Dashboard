c9ee4f7cbefc28f9092166b42618cf0b3345ed77
pniyongabo <patrickniyongabo@gmail.com>
2020-11-20 19:24:56 -0800

    removed manual CORS settings

--- nba-dashboard-server/server.js
+++ nba-dashboard-server/server.js
@@ -12,13 +12,6 @@ app.use(cors());
 var mockDataController = require('./mockDataController');
 var helperDataController = require('./helperDataController');
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    next();
-});
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
