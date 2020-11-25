f83caa9e21881787b9c1aca56bd25dc1dc45d3e0
pniyongabo <patrickniyongabo@gmail.com>
2020-11-24 22:12:53 -0800

    added components: team, live, schedules, standings

---
+++ nba-dashboard/src/components/live.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import {Doughnut} from 'react-chartjs-2';
+require("dotenv").config();
+
+export default class Teams extends Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      data: {},
+    }
+  }
+  
+
+  componentDidMount(){
+
+    const url = 'https://api-nba-v1.p.rapidapi.com/teams/league/standard';
+
+
+    // async function that gets teams list and header info from api
+    let loadResponse = async () => {
+        let response = await fetch(url, 
+          {"method": "GET",
+           "headers":
+           {
+            "x-rapidapi-host": process.env.REACT_APP_NONFREE_API_URL,
+            "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+            }
+          }).then(response=>response.json())
+            .then(json=>{
+            console.log(json);
+            this.setState({data: json, isLoaded: true})
+          }).catch(err=>{
+            console.log(err);
+          })
+
+    }
+
+    loadResponse();
+}
+
+
+load_data = () => {
+  return(
+       <table className="large-tables" id="teams">
+         <thead>
+            <tr>
+               <th>Name</th> 
+               <th>Abbr</th>
+               <th>Division</th>
+               <th>Conference</th>
+            </tr>
+         </thead>
+         <tbody>
+         {this.state.data.api.teams.map((item, i) => {
+           if (item.nbaFranchise == "1" && item.city != "Home" && item.city != "Away"){ // only dispaly NBA Teams
+            return (
+            <tr key={i}>
+              <td>{item.fullName}</td>
+              <td>{item.shortName}</td>
+              <td>{item.leagues.standard.divName}</td>
+              <td>{item.leagues.standard.confName}</td>
+            </tr>
+            )
+         }
+       })}
+         </tbody>
+       </table>
+     )
+
+}
+    
+  render() {
+    if(!this.state.isLoaded){
+      return (
+        <div>
+            <h1>
+              Loading teams data ...
+            </h1>
+        </div>
+      )
+    }
+
+    return (
+      <div className="align-center">
+          <h1>
+            NBA Teams
+          </h1>
+          
+
+          {this.load_data(this.state.data)}
+
+      </div>
+    );
+  }
+
+}
---
+++ nba-dashboard/src/components/team.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import {Doughnut} from 'react-chartjs-2';
+
+
+export default class Player extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+    }
+  }
+  
+  componentDidMount() {
+    console.log(this.props);
+    
+    this.getTeamFullName(this.props.location.state.data.teamId)
+      .then(res => this.setState({ 
+        data: this.props.location.state.data, 
+        playerId: this.props.match.params.id, 
+        isLoaded: true,
+        teamFullName: res
+      }))
+      .catch(err => console.log(err));
+    
+  }
+  
+  getTeamFullName = async (teamId) => {
+    const response = await fetch('http://localhost:8000/teams/mappings/' + teamId);
+    const body = await response.text();
+
+    if (response.status !== 200) {
+      return ""; 
+    }
+    
+    return body;
+  };
+  
+
+  
+  load_data = () => {
+    const playerImageURL = "https://nba-players.herokuapp.com/players/" + this.state.data.lastName + "/" + this.state.data.firstName;
+    const playerImageALT = "Headshot image of Nba Player " + this.state.data.lastName + " " + this.state.data.firstName;
+    return(
+      <div>
+        <div>
+        <img src={playerImageURL} alt={playerImageALT}/>
+        </div>
+        <div>
+           <h1>{this.state.data.firstName} {this.state.data.lastName}</h1>
+           <p>Weight (kg) : {this.state.data.weightInKilograms}</p>
+           <p>Height (m): {this.state.data.heightInMeters}</p>
+           <p>Born: {this.state.data.dateOfBirth}</p>
+           <p>College Team: {this.state.data.collegeName}</p>
+           <p>Current Team: {this.state.teamFullName}</p>
+        </div>
+      </div>
+       )
+       
+
+  }
+    
+  render() {
+    if(!this.state.isLoaded){
+      return (
+        <div>
+            <h1>
+              Loading player data ...
+            </h1>
+        </div>
+      )
+    }
+
+    return (
+      <div className="align-center">
+          {this.load_data(this.state.data)}
+      </div>
+    );
+  }
+
+}
